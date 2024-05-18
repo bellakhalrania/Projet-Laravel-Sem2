1c1be27d5d9760739fefa2d19e6ddf835db5075d
bellakhalrania <bellakhalrania12@gmail.com>
2024-05-18 16:13:27 +0100

    execute migration

--- database/migrations/2024_05_18_150001_create_abonnements_table.php
+++ database/migrations/2024_05_18_150001_create_abonnements_table.php
@@ -17,12 +17,14 @@ return new class extends Migration
             $table->text('prenom');
             $table->text('email');
             $table->integer('phone');
-            $table->text('lieu');
-            $table->date('datedebut');
-            $table->date('datefin');
-            $table->string('garedepart');
-            $table->string('garearrive');
+            $table->text('lieu'); 
             $table->string('image')->nullable();
+            $table->foreignId('trajet_id');
+            $table->foreignId('garedepart_id');
+            $table->foreignId('garearrive_id');
+            $table->foreign('trajet_id')->references('id')->on('trajets');
+            $table->foreign('garedepart_id')->references('id')->on('gares');
+            $table->foreign('garearrive_id')->references('id')->on('gares');
             $table->timestamps();
         });
     }
