bcebe7109393b6b5922c3eecd64a9cf4851429e3
bellakhalrania <bellakhalrania12@gmail.com>
2024-05-19 16:54:02 +0100

    modifier nav

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -28,28 +28,54 @@
                     </div>
                 </div>
             </li>
-
             <li class="nav-item">
-                <a class="nav-link" href="#" data-toggle="collapse" data-target="#collapseRoom" aria-expanded="true" aria-controls="collapseTwo">
-                    <span>{{ __('Room Management') }}</span>
+                <a class="nav-link" href="{{ url('/Abonnements') }}" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                    <span>{{ __('Gare') }}</span>
                 </a>
-                <div id="collapseRoom" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        
-                    </div>
-                </div>
-            </li>
-            
-            <li class="nav-item">
-                <a class="nav-link" href="#" data-toggle="collapse" data-target="#collapseBooking" aria-expanded="true" aria-controls="collapseTwo">
-                    <span>{{ __('Booking Management') }}</span>
-                </a>
-                <div id="collapseBooking" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
+                        <a class="collapse-item " href="{{ url('/Abonnements') }}"> <i class="fa fa-briefcase mr-2"></i> {{ __('list des abonnements') }}</a>
                        
                     </div>
                 </div>
             </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ url('/Abonnements') }}" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                    <span>{{ __('Trajet') }}</span>
+                </a>
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <a class="collapse-item " href="{{ url('/Abonnements') }}"> <i class="fa fa-briefcase mr-2"></i> {{ __('list des abonnements') }}</a>
+                       
+                    </div>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ url('/Abonnements') }}" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                    <span>{{ __('Bus') }}</span>
+                </a>
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <a class="collapse-item " href="{{ url('/Abonnements') }}"> <i class="fa fa-briefcase mr-2"></i> {{ __('list des abonnements') }}</a>
+                       
+                    </div>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ url('/Abonnements') }}" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                    <span>{{ __('Chauffeur') }}</span>
+                </a>
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <a class="collapse-item " href="{{ url('/Abonnements') }}"> <i class="fa fa-briefcase mr-2"></i> {{ __('list des abonnements') }}</a>
+                       
+                    </div>
+                </div>
+            </li>
+
+           
+            
+            
 
                      <!-- Nav Item  -->
              <li class="nav-item ">
