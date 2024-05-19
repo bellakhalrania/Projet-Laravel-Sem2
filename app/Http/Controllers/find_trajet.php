76746776b0e3d66a80fd453395c4c80be4595e54
bellakhalrania <bellakhalrania12@gmail.com>
2024-05-19 18:54:11 +0100

    find trajet

--- app/Http/Controllers/find_trajet.php
+++ app/Http/Controllers/find_trajet.php
@@ -17,51 +17,19 @@ class find_trajet extends Controller
         return view('find_trajets.index',compact('trajets'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+    public function store(Request $request){
+        $departure = $request->input('departure');
+        $destination = $request->input('destination');
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        if ($request->isMethod('POST')) {
+            $trajets = Trajet::where('departure', 'like', "%{$departure}%")
+                ->where('destination', 'like', "%{$destination}%")
+                ->get();
+        } else {
+            $trajets = [];
+        }
+        
+        return view('find_trajets.index', compact('trajets', 'departure', 'destination'));
     }
 }
