0291a9ee3d6466944bcf1ba937bb631266ed1dda
bellakhalrania <bellakhalrania12@gmail.com>
2024-05-19 16:17:47 +0100

    changer backend blade template

--- resources/views/backend/abonnements/index.blade.php
+++ resources/views/backend/abonnements/index.blade.php
@@ -1,58 +1,120 @@
 @extends('backend.app')
 @section('content') 
 
-<h1 class="title">Liste des offres</h1>
-<ul class="breadcrumbs">
-    <li><a href="#">Listes des offres</a></li>
-    <li class="divider">/</li>
-    <li><a href="#" class="active">Offres</a></li>
-</ul>
-<div  style="width: 1200px;">
-<div class="card" style="padding: 40px;">
-    <div class="content-data">
+<div class="container-fluid">
 
-<div class="head">
-    <h3></h3>
-    <div class="menu">
-        <button type="button" class="btn btn-info" routerLink="/admin/ajouter-gare" > Add offre </button>
-    </div>
-</div>
+    <!-- Page Heading -->
+   
 
-           
-    
-    
+    <!-- Content Row -->
+        <div class="card">
+            <div class="card-header py-3 d-flex">
+                <h6 class="m-0 font-weight-bold text-primary">
+                    {{ __('booking') }}
+                </h6>
+                <div class="ml-auto">
+                    @can('booking_create')
+                    <a href="{{ route('backend.abonnements.create') }}" class="btn btn-primary">
+                        <span class="icon text-white-50">
+                            <i class="fa fa-plus"></i>
+                        </span>
+                        <span class="text">{{ __('New abonnement') }}</span>
+                    </a>
+                    @endcan
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered table-striped table-hover datatable datatable-booking" cellspacing="0" width="100%">
+                        <thead>
+                            <tr>
+                                <th width="10">
 
+                                </th>
+                               
+        <th>Nom</th>
+        <th>Prénom</th>
+        <th>Email</th>
+        <th>Téléphone</th>
+        <th>Lieu</th>
+        <th>Trajet</th>
+        <th>Gare de départ</th>
+        <th>Gare d'arrivée</th>
+        <th>Options</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse($abonnements as $abonnement)
+                          
+                            <tr>
+                              <td></td>
+                              <td>{{ $abonnement->nom }}</td>
+                              <td>{{ $abonnement->prenom }}</td>
+                              <td>{{ $abonnement->email }}</td>
+                              <td>{{ $abonnement->phone }}</td>
+                              <td>{{ $abonnement->lieu }}</td>
+                              <td>{{ $abonnement->trajet->departure }}</td>
+                              <td>{{ $abonnement->trajet->destination }}</td>
+                              <td>{{ $abonnement->trajet->destination }}</td>
+                              <td>
+                                <a href="{{Route('Abonnements.show',$abonnement->id)}}" class="btn btn-outline-info btn-sm">Show</a>
+                                <a href="" class="btn btn-outline-warning btn-sm">Edit</a>
+                              </td>
+                            </tr>
+                            @empty
+                            <tr>
+                                <td colspan="9" class="text-center">{{ __('Data Empty') }}</td>
+                            </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    <!-- Content Row -->
 
-<table class="table">
-    <thead>
-        <tr>
-          <th>#</th>
-          <th></th>
-          <th>Nom</th>
-          <th>Lieu</th>
-          <th>Adresse</th>
-          <th>option</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        
-        <tr >
-          <td>1</td>
-          <td></td>
-          <td>
-            
-          </td>
-          <td></td>
-         
-         <td> <button type="button" style="border: none;" data-toggle="modal" data-target="#exampleModal"><img src="assets/admin/images/edite.png" class="icon-img"> </button> 
-            <button type="button" style="border: none;" ><img src="assets/admin/images/delete.png" class="icon-img" ></button></td>
-        </tr>
-        
-      </tbody>
-  </table>
-</div>
-</div>
 </div>
 @endsection
+
+
+@push('script-alt')
+<script>
+    $(function () {
+  let dtButtons = $.extend(true, [], $.fn.dataTable.defaults.buttons)
+  let deleteButtonTrans = 'delete selected'
+  let deleteButton = {
+    text: deleteButtonTrans,
+    
+    className: 'btn-danger',
+    action: function (e, dt, node, config) {
+      var ids = $.map(dt.rows({ selected: true }).nodes(), function (entry) {
+          return $(entry).data('entry-id')
+      });
+      if (ids.length === 0) {
+        alert('zero selected')
+        return
+      }
+      if (confirm('are you sure ?')) {
+        $.ajax({
+          headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+          method: 'POST',
+          url: config.url,
+          data: { ids: ids, _method: 'DELETE' }})
+          .done(function () { location.reload() })
+      }
+    }
+  }
+  dtButtons.push(deleteButton)
+  $.extend(true, $.fn.dataTable.defaults, {
+    order: [[ 1, 'asc' ]],
+    pageLength: 50,
+  });
+  $('.datatable-booking:not(.ajaxTable)').DataTable({ buttons: dtButtons })
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function(e){
+        $($.fn.dataTable.tables(true)).DataTable()
+            .columns.adjust();
+    });
+})
+</script>
+@endpush
 
